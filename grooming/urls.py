bded37517ece4ecc5264adb41421011ca6abec55
Laura Fox <laurafox91@gmail.com>
2024-08-11 00:15:26 +0000

    Add services and price list, update bootstrap CSS

--- grooming/urls.py
+++ grooming/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('contact/', views.about, name='contact'),
+    path('contact/', views.contact, name='contact'),
     
 ]
