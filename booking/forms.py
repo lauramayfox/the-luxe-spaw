3750be253157c3de5c53fbef7fbeb4ee840e6274
Laura Fox <laurafox91@gmail.com>
2024-08-11 22:31:01 +0000

    Add crispy-bootstrap5 to requirements.txt

--- booking/forms.py
+++ booking/forms.py
@@ -1,7 +1,5 @@
-from .models import Booking, UserProfile, GROOMING_CHOICES, TIME_CHOICES
+
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User, TextInput, NumberInput
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
 from django import forms
 
